ccfeca5374a50a7c3d17e0af725f091a20aeebea
sankadilishan <sankadilishan9686@gmail.com>
2024-03-25 08:44:32 +0530

    updated changes

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -6,11 +6,11 @@ import HeaderRight from "./HeaderRight";
 
 function Dashboard() {
     return ( 
-        <div className="w-full">
+        <div className="w-full min-w-full">
             <video loop autoPlay muted className="-z-10 absolute" id="bg-video">
                 <source src={bg_video} type="video/mp4"/>
            </video>
-           <div className="flex justify-center align-middle pt-10 pl-10 text-white">
+           <div className="flex justify-center flex-wrap align-middle pt-10 pl-10 text-white">
             <HeaderLeft/>
             <HeaderRight />
            </div>
